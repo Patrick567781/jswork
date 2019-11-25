cc0c6b114180dde532c5f29ffc3c060805a67ddd
Patrick567781 <1324032077@qq.com>
2019-11-25 21:33:05 +0800

    work12

--- work12/calendar.js
+++ work12/calendar.js
@@ -1,6 +1,6 @@
 function calendar(y) {
     var w = new Date(y,0).getDay();
-    var html = '<div class="box>';
+    var html = '<div class="box">';
     for(var m = 1;m <=12;++m){
         html +='<table>';
         html +='<tr class="title"><th colspan="7">' + y + '年' +  m  + '月</th></tr>';
@@ -21,6 +21,6 @@ function calendar(y) {
         }
         html += '</table>';
     }
-    html += '</div>;'
+    html += '</div>';
     return html;
 }
